0e721775df00c04ffaba9245f48cdaa407dd11ed
shiv-k-sharma <kumarshivsharma12@gmail.com>
2018-10-06 22:37:12 +0530

    checking for non-empty deviceTokken and get rid of promise nesting

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 //  this is to initialize the app to use "Cloud Functions"
 admin.initializeApp(functions.config().firebase);
 
 //  on friend request sent
 exports.sendNotificationOnFriendRequest = functions.database
-  .ref("/Notifications/{receiver_id}/{push_id}")
+  .ref('/Notifications/{receiver_id}/{push_id}')
   .onWrite((event, context) => {
     //  getting receiverUid and senderUid from "notifications" reference
     const receiverId = context.params.receiver_id;
@@ -20,51 +20,54 @@ exports.sendNotificationOnFriendRequest = functions.database
     //  grabbing sender's id
     const senderId = event.after.val().from;
 
+    let senderUsername;
+    let sendersImageUrl;
     //  sender's reference
-    const sendersReference = admin
+    return admin
       .database()
       .ref(`/Users/${senderId}`)
-      .once("value");
+      .once('value')
+      .then(info => {
+        senderUsername = info.val().user_name;
+        sendersImageUrl = info.val().user_image;
 
-    return sendersReference.then(info => {
-      const senderUsername = info.val().user_name;
-      const sendersImageUrl = info.val().user_image;
-
-      //  getting receiver's device token
-      const receiversReference = admin
-        .database()
-        .ref(`/Users/${receiverId}`)
-        .once("value");
-      return receiversReference.then(receiversData => {
-        const deviceToken = receiversData.val().device_token;
+        // returning reference to receiverUser node to continue chaining
+        return admin
+          .database()
+          .ref(`/Users/${receiverId}`)
+          .once('value');
+      })
+      .then(receiversData => {
+        //  getting receiver's device token
+        let deviceToken = receiversData.val().device_token;
 
         let payload = {
           data: {
-            title: "New Friend Request",
+            title: 'New Friend Request',
             body: `${senderUsername} has sent you a friend request `,
-            icon: "default",
-            clickAction: "openProfileActivity",
-            type: "sentRequest",
+            icon: 'default',
+            clickAction: 'openProfileActivity',
+            type: 'sentRequest',
             senderId,
             imageUrl: sendersImageUrl
           }
         };
-        return admin
-          .messaging()
-          .sendToDevice(deviceToken, payload)
-          .then(response => {
-            return console.log("sendNotificationOnFriendRequest sent");
-          })
-          .catch(e => {
-            return console.log(e);
-          });
-      });
-    });
+
+        // send data if deviceToken exists
+        if (deviceToken) {
+          //  sending the data to the receiver
+          return admin.messaging().sendToDevice(deviceToken, payload);
+        } else {
+          return new Error('deviceToken not found or receiver logged out');
+        }
+      })
+      .then(response => console.log('sendNotificationOnFriendRequest sent'))
+      .catch(e => console.log(e));
   });
 
 //    on message sent
 exports.sendNotificationOnMessageSent = functions.database
-  .ref("/MessageReferenceForNotifications/{senderId}/{receiverId}/{pushId}")
+  .ref('/MessageReferenceForNotifications/{senderId}/{receiverId}/{pushId}')
   .onWrite((event, context) => {
     let senderId = context.params.senderId;
     let receiverId = context.params.receiverId;
@@ -77,47 +80,52 @@ exports.sendNotificationOnMessageSent = functions.database
 
     //  to shouldn't be null
     if (receiverId) {
-      //  grabbing the device token
+      let message;
       return admin
         .database()
-        .ref(`/Users/${receiverId}/device_token`)
-        .once("value")
-        .then(response => {
-          let deviceToken = response.val();
+        .ref(`/Messages/${senderId}/${receiverId}/${pushId}`)
+        .once('value')
+        .then(messageData => {
+          message = messageData.val().message;
 
-          //  getting the sender's image
+          // returning ref to "Users" node to continue chaining
           return admin
             .database()
-            .ref(`/Users/${senderId}`)
-            .once("value")
-            .then(response => {
-              let sendersImageUrl = response.val().user_image;
-              let username = response.val().user_name;
+            .ref('/Users')
+            .once('value');
+        })
+        .then(users => {
+          let deviceToken = users.child(receiverId).val().device_token;
 
-              let payload = {
-                data: {
-                  title: "New Message",
-                  body: `${username} has sent you a new message`,
-                  clickAction: "openMainActivity",
-                  type: "message",
-                  imageUrl: sendersImageUrl,
-                  senderId,
-                  pushId
-                }
-              };
+          //  getting the sender's image
+          let sendersImageUrl = users.child(senderId).val().user_image;
+          let username = users.child(senderId).val().user_name;
 
-              //  sending the data to the receiver
-              return admin
-                .messaging()
-                .sendToDevice(deviceToken, payload)
-                .then(response => {
-                  return console.log("sendNotificationOnMessageSent sent");
-                })
-                .catch(err => {
-                  return console.log(err);
-                });
-            });
-        });
+          let payload = {
+            data: {
+              title: 'New Message',
+              body: `${username} has sent you a new message`,
+              clickAction: 'openMainActivity',
+              type: 'message',
+              imageUrl: sendersImageUrl,
+              receiverId,
+              username,
+              senderId,
+              message,
+              pushId
+            }
+          };
+
+          // send data if deviceToken exists
+          if (deviceToken) {
+            //  sending the data to the receiver
+            return admin.messaging().sendToDevice(deviceToken, payload);
+          } else {
+            return new Error('deviceToken not found or user logged out');
+          }
+        })
+        .then(response => console.log('sendNotificationOnMessageSent sent'))
+        .catch(err => console.log(err));
     } else {
       return 0;
     }
@@ -125,7 +133,7 @@ exports.sendNotificationOnMessageSent = functions.database
 
 //    for accepting friend request
 exports.sendNotificationOnAcceptingRequest = functions.database
-  .ref("/FriendsReferenceForNotification/{receiver_id}/{sender_id}")
+  .ref('/FriendsReferenceForNotification/{receiver_id}/{sender_id}')
   .onWrite((event, context) => {
     if (!event.after.exists()) {
       return 0;
@@ -135,45 +143,39 @@ exports.sendNotificationOnAcceptingRequest = functions.database
     const receiverId = context.params.receiver_id;
     const request = event.after.val().request;
 
-    if (request === "accepted") {
+    if (request === 'accepted') {
       return admin
         .database()
-        .ref(`/Users/${senderId}`)
-        .once("value")
-        .then(snapshot => {
-          const imageUrl = snapshot.val().user_image;
-          const deviceToken = snapshot.val().device_token;
+        .ref(`/Users`)
+        .once('value')
+        .then(users => {
+          const imageUrl = users.child(senderId).val().user_image;
+          const deviceToken = users.child(senderId).val().device_token;
+          const receiversUsername = users.child(receiverId).val().user_name;
 
-          //  getting receiver's username
-          let receiversReference = admin
-            .database()
-            .ref(`/Users/${receiverId}`)
-            .once("value");
-          return receiversReference.then(receiversData => {
-            const receiversUsername = receiversData.val().user_name;
+          let payload = {
+            data: {
+              title: 'New Message',
+              body: `${receiversUsername} accepted your friend request `,
+              clickAction: 'openMainActivity',
+              type: 'acceptRequest',
+              imageUrl: imageUrl,
+              senderId
+            }
+          };
 
-            let payload = {
-              data: {
-                title: "New Message",
-                body: `${receiversUsername} accepted your friend request `,
-                clickAction: "openMainActivity",
-                type: "acceptRequest",
-                imageUrl: imageUrl,
-                senderId
-              }
-            };
-
-            return admin
-              .messaging()
-              .sendToDevice(deviceToken, payload)
-              .then(response => {
-                return console.log("sendNotificationOnAcceptingRequest sent");
-              })
-              .catch(err => {
-                return console.log(err);
-              });
-          });
-        });
+          // send data if deviceToken exists
+          if (deviceToken) {
+            //  sending the data to the receiver
+            return admin.messaging().sendToDevice(deviceToken, payload);
+          } else {
+            return new Error('deviceToken not found or receiver logged out');
+          }
+        })
+        .then(response =>
+          console.log('sendNotificationOnAcceptingRequest sent')
+        )
+        .catch(err => console.log(err));
     } else {
       return 0;
     }
